30d85eb27b86167286dae7176a18fbc5ba0fa164
joytun21 <istiqwamancikalang@gmail.com>
2025-06-06 03:40:56 +0700

    Delete modules/reseller directory

--- modules/reseller/index.js
+++
@@ -1,7 +0,0 @@
-// modules/reseller/index.js
-module.exports = (bot, db, adminIds) => {
-  require('./tambah')(bot, db, adminIds);
-  require('./hapus')(bot, db, adminIds);
-  require('./list')(bot, db, adminIds);
-  require('./saldo')(bot, db, adminIds);
-};
