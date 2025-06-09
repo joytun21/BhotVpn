c33ed10d7976547da276b3e32bf3d38ad570675e
joytun21 <istiqwamancikalang@gmail.com>
2025-06-09 21:59:59 +0700

    Delete logger.js

--- logger.js
+++
@@ -1,8 +0,0 @@
-// logger.js
-const logger = {
-  info: (...args) => console.log('[INFO]', ...args),
-  warn: (...args) => console.warn('[WARN]', ...args),
-  error: (...args) => console.error('[ERROR]', ...args),
-};
-
-module.exports = logger;
