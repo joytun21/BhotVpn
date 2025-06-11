53165ab4383a8e285cf05af9d68a347333e7c2fa
joytun21 <istiqwamancikalang@gmail.com>
2025-06-12 06:55:02 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const raw = fs.readFileSync(path.join(__dirname, '.vars.json'), 'utf8');
-const config = JSON.parse(raw);
-
-module.exports = config;
